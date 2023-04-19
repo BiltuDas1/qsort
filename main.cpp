c68639b711c5bab9a49bb4ebd333ae038274d304
Biltu Das <billionto@gmail.com>
2023-04-19 22:58:56 +0530

    Last Beta 0.1 Update of base class

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 // Initialization
 class init
 {
-    protected:
-        inline string read_path_variable() {
-            return getenv("PATH");
-        }
+    inline string read_path_variable() {
+        return getenv("PATH");
+    }
 
+    protected:
+        // Dependencies
+        void init_required(){
+
+        }
 };
 
 // Main Program
@@ -48,6 +52,9 @@ class base : protected init
                 }
             // If no arguments passed then do main operation
             } else {
+                // Initializing Variables/Settings
+                init::init_required();
+
                 // Listing all folder/files
                 for(auto const& files : fs::directory_iterator{fs::current_path()}) 
                 {
@@ -62,15 +69,30 @@ class base : protected init
                             *extension = tempstr->substr(tempstr->find_last_of(".") + 1);
 
                             // Check If extension exist into configuration
-                            if(exclude::extensions->find(*extension) != string::npos){
+                            if(exclude::extensions->find(*extension) != string::npos)
                                 continue;
-                            }
 
+                            // Check if extension exist into Catagories
+                            string *mvpath = new string;
+                            if(json::documents->find(*extension) != string::npos)
+                                *mvpath = *path::documents;
 
+                            else if(json::pictures->find(*extension) != string::npos)
+                                *mvpath = *path::pictures;
 
-                            delete extension;
+                            else if(json::musics->find(*extension) != string::npos)
+                                *mvpath = *path::music;
+
+                            else if(json::videos->find(*extension) != string::npos)
+                                *mvpath = *path::videos;
+                            else
+                                *mvpath = *path::general;
+                            
+                            fs::rename(pathfile, *mvpath + "/" + *tempstr);
+
+                            delete extension, mvpath;
                         } else {
-                            // If no extension and not found into Exclude list then move to General
+                            // If no extension and file not found into Exclude list then move to General
                             if(exclude::filenames->find(*tempstr) == string::npos){
                                 fs::path temp_path = *path::general + "/" + *tempstr;
                                 fs::rename(pathfile, temp_path);
@@ -85,6 +107,7 @@ class base : protected init
             delete ver, vertype, tempstr;
             delete exclude::extensions, exclude::filenames;
             delete path::documents, path::general, path::music, path::pictures, path::videos;
+            delete json::documents, json::musics, json::pictures, json::videos;
         }
 };
 
